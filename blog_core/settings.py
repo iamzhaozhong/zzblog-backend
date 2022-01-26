c1cec568a837420957e1f3f6f42134772f7d1179
Zhao Zhong <zhzhong@sas.upenn.edu>
2022-01-26 16:11:03 -0700

    fixing the timezone

--- blog_core/settings.py
+++ blog_core/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC-7"
+TIME_ZONE = "UTC"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
