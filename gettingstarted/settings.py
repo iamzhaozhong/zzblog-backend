ec33337b859be33b14faca5148e09b6da0d8b365
Zhao Zhong <zhzhong@sas.upenn.edu>
2022-01-26 08:08:12 -0700

    created the development branch

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = "/static/"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
 
